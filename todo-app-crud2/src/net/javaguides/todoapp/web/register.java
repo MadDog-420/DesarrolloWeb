db5a39344f0d0b8c8010123719bf0899409b4a40
MadDog-420 <u18211928@utp.edu.pe>
2020-10-20 23:01:14 -0500

    Primus

---
+++ todo-app-crud2/src/net/javaguides/todoapp/web/register.java
@@ -0,0 +1,77 @@
+package net.javaguides.todoapp.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.javaguides.todoapp.dao.UserDao;
+import net.javaguides.todoapp.model.User;
+
+/**
+ * Servlet implementation class register
+ */
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDao userDao;
+
+	public void init() {
+		userDao = new UserDao();
+	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("register/register.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		registerUser(request, response);
+	}
+	
+	private void registerUser(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		User employee = new User();
+		employee.setFirstName(firstName);
+		employee.setLastName(lastName);
+		employee.setUsername(username);
+		employee.setPassword(password);
+
+		try {
+			int result = userDao.registerEmployee(employee);
+			if(result == 1) {
+				request.setAttribute("NOTIFICATION", "Usuario registrado!");
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("register/register.jsp");
+		dispatcher.forward(request, response);
+	}
+}
