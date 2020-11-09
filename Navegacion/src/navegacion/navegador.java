166b2340ae6be8de03851aab7820852376f84b51
MadDog-420 <u18211928@utp.edu.pe>
2020-11-08 23:26:34 -0500

    Damn

--- Navegacion/src/navegacion/navegador.java
+++ Navegacion/src/navegacion/navegador.java
@@ -2,26 +2,72 @@ package navegacion;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ManagedBean (name="consulta")
 @RequestScoped
-public class navegador implements Serializable{
+@SessionScoped
+public class navegador{
+	
+	private String page;
+	private String content;
 
-	public String metodo1() {
-		return "/page.xhtml?page=sonrisa";
+	public String outcome(){
+        
+        FacesContext fc = FacesContext.getCurrentInstance();
+        this.page = getPageParam(fc);
+        return "page";
+    
 	}
-	
-	public String metodo2() {
-		return "/page.xhtml?page=ui";
+
+    //get value from "f:param"
+    public String getPageParam(FacesContext fc){
+        
+        Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
+        String result = params.get("page");
+        switch(result) {
+        case "sonrisa":
+        	this.content="<img src='https://res.cloudinary.com/mundo/image/upload/f_auto,q_auto/h_338,w_400/v1559233282/shutterstock-d%C3%ADa-sonrisa-2_bxgc7e.jpg?cld_params=f_auto,q_auto/h_338,w_400'>";
+        	break;
+        case "Ejemplos de elementos UI":
+        	this.content="<input type=\"checkbox\" id=\"vehicle1\" name=\"vehicle1\" value=\"Bike\">"
+        			+ "<label for=\"vehicle1\"> I have a bike</label></br></br>"
+        			+ "<input type=\"radio\" id=\"male\" name=\"gender\" value=\"male\">\r\n" + 
+        			"<label for=\"male\">Male</label></br></br>"
+        			+ "<button type=\"button\">Click Me!</button>";
+        	break;
+        case "Facelet":
+        	this.content="<h2>Esto es un facelet</h2>";
+        	break;
+        }
+        return result;
+        
+    }
+
+	public String getPage() {
+		return page;
 	}
-	
-	public String metodo3() {
-		return "/page.xhtml?page=facelet";
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
 	}
 	
 }
